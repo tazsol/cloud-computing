d473cf926f0d2a8346397852885596d19a72bc0e
tazsol <tzsolt92@gmail.com>
2020-10-19 13:12:24 +0200

    corrections based on discussion
    Rewrote user script to be simpler, included sources

--- sprint_1.tf
+++ sprint_1.tf
@@ -70,38 +70,18 @@ resource "exoscale_instance_pool" "instancepool-project" {
   user_data          = <<EOF
 #!/bin/bash
 
-set -e
-
+# setting to avoid configuration prompts
 export DEBIAN_FRONTEND=noninteractive
 
-# region Install Docker
-apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+# convenience script for docker installation - source: https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-# endregion
-
-# region Launch containers
-
-# Run the load generator
+# Run the load generator through docker - source: https://github.com/FH-Cloud-Computing/http-load-generator
 docker run -d \
   --restart=always \
   -p 8080:8080 \
   janoszen/http-load-generator:1.0.1
-# endregion
 EOF
 }
 
